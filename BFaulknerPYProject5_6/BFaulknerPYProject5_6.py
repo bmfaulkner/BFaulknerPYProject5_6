e16482d4f5b8112f208f8b25ecc4ed3209419a5f
Brandon Faulkner <faulknerb48@gmail.com>
2021-04-28 23:22:30 -0500

    First Commit For The Second Repository For Project 7.1

---
+++ BFaulknerPYProject5_6/BFaulknerPYProject5_6.py
@@ -0,0 +1,44 @@
+import geocoder
+
+#Forward Geocoding
+print("Forward Example")
+g = geocoder.komoot('Phoenix, AZ')
+print(g.latlng)
+print(g.country)
+print("\n")
+
+#Reverse Geocoding
+print("Reverse Example")
+g = geocoder.arcgis([51.5074, 0.1278], method='reverse')
+print(g.city)
+print(g.state)
+print(g.country)
+print("\n")
+
+#IP Address 1
+print("IP Address Example 1")
+g = geocoder.ip('8.8.8.8')
+print(g.ip)
+print(g.postal)
+print(g.latlng)
+print(g.city + " " + g.state)
+print("\n")
+
+#IP Address 2
+print("IP Address Example 2")
+g = geocoder.ip('me')
+print(g.ip)
+print(g.postal)
+print(g.latlng)
+print(g.city + " " + g.state)
+print("\n")
+
+#House Address
+print("House Address")
+g = geocoder.komoot("1600 Pennsylvania Avenue NW, Washington D.C.")
+print(g.housenumber)
+print(g.postal)
+print(g.street)
+print(g.city)
+print(g.state)
+print(g.country)
